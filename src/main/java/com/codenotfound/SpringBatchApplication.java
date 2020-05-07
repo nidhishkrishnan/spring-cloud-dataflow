921cc24512d22dc20c9ca7fcfc5678dbca7a8d1f
Nidhish Krishnan <nidhishkrishnan@gmail.com>
2020-05-07 17:58:06 +0200

    Initial Commit

---
+++ src/main/java/com/codenotfound/SpringBatchApplication.java
@@ -0,0 +1,18 @@
+package com.codenotfound;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.dataflow.server.EnableDataFlowServer;
+import org.springframework.cloud.task.configuration.EnableTask;
+
+@EnableTask
+@EnableDataFlowServer
+@EnableBatchProcessing
+@SpringBootApplication
+public class SpringBatchApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBatchApplication.class, args);
+  }
+}
